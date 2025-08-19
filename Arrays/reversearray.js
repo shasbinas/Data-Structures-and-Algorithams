cd8ccaeaa748be46894f56ae74e9bcab86f576ed
shasbinas <shasbinas1@gmail.com>
2025-08-19 15:35:20 +0530

    reversearray comment changed

--- Arrays/reversearray.js
+++ Arrays/reversearray.js
@@ -1,3 +1,5 @@
+
+/* reverse array */
 function reverseArray(arr) {
     return arr.reverse();
 }
@@ -5,3 +7,4 @@ function reverseArray(arr) {
 let nums = [1, 2, 3, 4, 5];
 console.log("Original:", nums);
 console.log("Reversed:", reverseArray([...nums]));
+/*  */
