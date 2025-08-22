39bcb36b214924f2d7fc6dc6c74263c987877ff1
shasbinas <shasbinas1@gmail.com>
2025-08-22 14:11:30 +0530

    arraymethodcompleted

---
+++ Arrays/arraymethods.js
@@ -0,0 +1,35 @@
+/* push() */
+// let arr = [1, 2, 3];
+// arr.push(4);  
+// console.log(arr);
+
+/* pop() */
+// let arr=[1,2,3,4,22];
+// arr.pop(5);
+// console.log(arr);
+
+/* shift */
+// let arr =[1,2,3,4,5]
+// let modified=arr.shift();
+// console.log(arr);
+// console.log(modified);
+/*  unshift */
+// let arr = [2, 3];
+// arr.unshift(11, 1);
+// console.log(arr); 
+/* slice(start, end) */
+// let arr = [10, 20, 30, 40, 50];
+// let sliced = arr.slice(1, 4);
+// console.log(sliced);
+// console.log(arr);  
+/*splice */
+// let arr = [10, 20, 30, 40, 50];
+// arr.splice(1, 2);
+// console.log(arr); // [10, 40, 50]
+// arr.splice(1, 0, 15, 25);
+// console.log(arr); // [10, 15, 25, 40, 50]
+// arr.splice(2, 1, 100);
+// console.log(arr); // [10, 15, 100, 40, 50]
+
+
+
