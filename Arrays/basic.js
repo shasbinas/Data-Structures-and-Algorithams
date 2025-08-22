ca19909bbf84c101faafeccbacd9b9d4872653c4
shasbinas <shasbinas1@gmail.com>
2025-08-22 14:17:49 +0530

    basic programe completed

---
+++ Arrays/basic.js
@@ -0,0 +1,28 @@
+
+/*Traversal (visiting elements) */
+// let arr = [10, 20, 30, 40];
+
+// // For loop
+// for (let i = 0; i < arr.length; i++) {
+//   console.log(arr[i]);
+// }
+
+// // For...of loop
+// for (let num of arr) {
+//   console.log(num);
+// }
+
+// // forEach
+// arr.forEach((value, index) => {
+//   console.log(index, value);
+// });
+/* Declaration */
+
+// Using square brackets (most common)
+let arr1 = [10, 20, 30];
+
+// Using Array constructor
+let arr2 = new Array(10, 20, 30);
+
+console.log(arr1);
+console.log(arr2);
